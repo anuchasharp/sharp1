7d0ebb13d459e6c7e81edcc492e0b8c6bbdd07d2
anuchasharp <79188443+anuchasharp@users.noreply.github.com>
2021-04-07 14:43:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print ("Test")
+from flask import Flask
+app = Flask(_name_)
+
+@app.route("/")
+def Home():
+    return "<h1>555</h1>"
+
+
+if _name_ == '_main_':
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
